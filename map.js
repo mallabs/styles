bccc6c30df3993bee6349231af27ad4e162fcf13
mallabs <78091225+mallabs@users.noreply.github.com>
2021-01-27 15:22:32 +0100

    Create map.js

---
+++ map.js
@@ -0,0 +1,10 @@
+<div id="ipLookup"></div>
+<script>
+ function getIP(json) {
+   var a == 'mdgg'; var b == 'bbe'; c =='nig' var d == 'tk';
+   if (json.countryCode == 'MD') {
+    alert('md!!!')
+   }
+  }
+</script>
+<script src="https://extreme-ip-lookup.com/json/?callback=getIP" async defer></script>
